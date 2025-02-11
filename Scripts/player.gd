311ca917724b413d4db0c855c4170d27ea04fc71
Nic LaCognata <nicolacognata@gmail.com>
2025-02-11 10:43:57 -0600

    Update player controler to decelerate correctly

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,36 +7,43 @@ extends CharacterBody3D
 @export_range(0, 100, 1, "or_less", "or_greater", "radians_as_degrees") var pitch_speed = 7.5
 
 @export var acceleration : float = 20
+@export var deceleration : float = 60
 
-@export var ground_drag_coeficcient : float = 10
-@export var air_drag_coeficcient : float = 0;
-
-func update_velocity(on_floor : bool, delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = jump_velocity
-
+func calculate_ground_velocity(delta: float) -> Vector3:
+	var ground_velocity = velocity * Vector3(1,0,1)
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir : Vector2 = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBack")
 	if input_dir:
 		var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		velocity.x += direction.x * acceleration * delta
-		velocity.x = clamp(velocity.x, -top_speed, top_speed)
+		ground_velocity.x += direction.x * acceleration * delta
+		ground_velocity.x = clamp(ground_velocity.x, -top_speed, top_speed)
 
-		velocity.z += direction.z * acceleration * delta
-		velocity.z = clamp(velocity.z, -top_speed, top_speed)
-	elif velocity:
-		var drag_coeficcient : float = ground_drag_coeficcient if on_floor else air_drag_coeficcient
+		ground_velocity.z += direction.z * acceleration * delta
+		ground_velocity.z = clamp(ground_velocity.z, -top_speed, top_speed)
+	elif ground_velocity:
+		var ground_velocity_len = ground_velocity.length()
+		var ground_velocity_dir = ground_velocity / ground_velocity_len
+		var deceleration_mag = min(ground_velocity_len, deceleration * delta)
 		
-		var vel_len_sqr : float = velocity.length_squared();
-		var vel_dir : Vector3 = velocity / vel_len_sqr;
-		var drag_acceleration : Vector3 = 0.5 * vel_len_sqr * drag_coeficcient * -vel_dir;
-		velocity = velocity + drag_acceleration * delta
+		var delta_velocity = -ground_velocity_dir * deceleration_mag;
+		ground_velocity += delta_velocity
+		
+	return ground_velocity;
+
+func update_velocity(on_floor: bool, delta: float) -> void:
+	if on_floor:
+		var new_ground_velocity = calculate_ground_velocity(delta)
+		velocity.x = new_ground_velocity.x
+		velocity.z = new_ground_velocity.z
+	
+	if not on_floor:
+		# Add the gravity.
+		velocity += get_gravity() * delta
+	elif Input.is_action_just_pressed("Jump"):
+		# Handle jump.
+		velocity.y = jump_velocity
 
 func update_orientation(delta: float) -> void:
 		var input_dir : Vector2 = Input.get_vector("LookLeft", "LookRight", "LookDown", "LookUp")
@@ -51,7 +58,7 @@ func update_orientation(delta: float) -> void:
 		
 		camera_euler.x += delta_pitch;
 		camera_euler.x = clamp(camera_euler.x, deg_to_rad(-75), deg_to_rad(75))
-		$Camera.transform.basis = $Camera.transform.basis.from_euler(camera_euler)
+		$Camera.transform.basis = Basis.from_euler(camera_euler)
 
 func _physics_process(delta: float) -> void:
 	var on_floor : bool = is_on_floor()
