226f7a56440e1b049c844d3f779aab99048d756e
Nic LaCognata <nicolacognata@gmail.com>
2025-03-09 15:00:19 -0500

    Add Interactible Component Scene

--- Scripts/interact_label.gd
+++ Scripts/interact_label.gd
@@ -1,22 +1,25 @@
 extends Label3D
 
 @onready var player_node = get_tree().get_nodes_in_group("Player")[0]  
+@onready var camera_node = player_node.get_node("Camera")
+@onready var boom_length : float = (transform.origin).length()
+
 @export var interact_distance : float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.visible = false
+	pass
 	
+func orient_label():
+	var pivot_pos : Vector3 = get_parent_node_3d().global_transform.origin
+	var camera_pos : Vector3 = camera_node.global_transform.origin
+	
+	var offset = (camera_pos - pivot_pos).normalized() * boom_length
+	
+	global_transform.origin = pivot_pos + offset
+
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
-	var parent_pos_xz : Vector3 = get_parent_node_3d().transform.origin * Vector3(1,0,1)
-	var player_pos_xz : Vector3 = player_node.transform.origin * Vector3(1,0,1)
-
-	var dist_sqr = (player_pos_xz - parent_pos_xz).length_squared()
-
-	var in_range = dist_sqr < interact_distance * interact_distance
-	if in_range:
-		self.visible = true
-	else:
-		self.visible = false
+	orient_label()
