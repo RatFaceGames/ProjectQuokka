d3a58e1368093629d8a4a784f90efad945ed672d
Ben Caldwell <caldwellb@uchicago.edu>
2025-04-06 15:25:54 -0500

    slop detector added

---
+++ Scenes/Components/slop_detector.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@export var on_slop : Callable
+signal slop_touched(slop : SlopChunk)
+
+func is_slop(node : Node3D) -> bool:
+	return node is SlopChunk
+
+func get_slop_inside() -> Array[Node3D]:
+	return get_overlapping_bodies().filter(is_slop)
+
+func slop_entered(body : Node3D):
+	emit_signal("slop_touched")
