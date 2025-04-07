fe04244faa5dbb2bc1f984d1a255b05a0369085d
Nic LaCognata <Nic LaCognata>
2025-04-06 19:03:45 -0500

    Start swapping slop gun model for glb

--- Scripts/SlopGun.gd
+++ Scripts/SlopGun.gd
@@ -9,13 +9,14 @@ var chunk_prefab : PackedScene = preload("uid://0itkq5xr37bp") #SlopCunk.tcsn
 
 func _ready() -> void:
 	# Setup recoil animation, driven by damped_trigger_strength
-	$RecoilSlidePlayer.play("SlopGunPushback")
-	$RecoilSlidePlayer.speed_scale = 0
+	#$RecoilSlidePlayer.play("SlopGunPushback")
+	#$RecoilSlidePlayer.speed_scale = 0
+	pass
 
 func _physics_process(delta: float) -> void:
 	#todo - spring damping on trigger strength
 	var trigger_strength : float = Input.get_action_strength("Shoot")
-	$RecoilSlidePlayer.seek(trigger_strength, true)
+	#$RecoilSlidePlayer.seek(trigger_strength, true)
 
 func fire(deltaTime: float) -> void:
 	var chunk : SlopChunk = chunk_prefab.instantiate()
@@ -24,6 +25,6 @@ func fire(deltaTime: float) -> void:
 	var vertical_angle = (randf() * vertical_spread_cone * 2) - vertical_spread_cone
 	
 	var spread_matrix = transform.basis.from_euler(Vector3(horizontal_angle, vertical_angle, 0))
-	chunk.global_transform.origin = $GunModel/SlopEmitter.global_transform.origin
-	chunk.global_transform.basis = $GunModel/SlopEmitter.global_transform.basis * spread_matrix;
+	chunk.global_transform.origin = $SlopEmitter.global_transform.origin
+	chunk.global_transform.basis = $SlopEmitter.global_transform.basis * spread_matrix;
 	chunk.linear_velocity = -chunk.global_basis.z * slop_exit_speed
